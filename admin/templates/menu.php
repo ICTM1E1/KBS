648b312d9c454562acb915de31bb9afcc53f32e1
Richard van den Hoorn <hoornrichard@hotmail.com>
2013-01-08 11:52:56 +0100

    meldingen bijgemaakt menu-items

--- admin/templates/menu.php
+++ admin/templates/menu.php
@@ -30,22 +30,35 @@ function haalartikeltitelop($id) {
 	}
 
 }
+$style = '';
+$statusText = "";
 
 if (isset ( $_POST ['submit'] )) {
 	$article_id = $_POST ['article_id'];
 	$id = $_POST ['id'];
 	
-	$sth = $dbh->query ( "UPDATE menu_item SET article_id='$article_id' WHERE id=$id" );
+	$sth = $dbh->query ( "UPDATE menu_item SET article_id=$article_id WHERE id=$id" );
 	// $sth->bindParam(":article_id", $article_id);
 	// $sth->bindParam(":parent_item", $parent_item);
 	// $sth->bindParam(":child_item", $child_item);
-	$sth->execute ();
+	$result = $sth->execute ();
+	if($result == true){
+		$style = 'message_success';
+		$statusText = "Wijziging succesvol opgeslagen.";
+	}
+	else{
+		$style = 'message_error';
+		$statusText = "Er is een fout opgetreden tijdens het opslaan van de wijziging, de wijziging is niet doorgevoerd!";
+	}
+	
 	header ( "Location: " );
 	exit ();
 }
 
 ?>
-
+<div class="<?php echo $style; ?>">
+	<p><?php echo $statusText; ?></p>
+</div>
 
 <table class="hover">
 	<tr>
@@ -57,35 +70,38 @@ if (isset ( $_POST ['submit'] )) {
 <?php
 foreach ( $res as $row ) { // Loop door SQL-resultaten
 	?>
-    <form action="" method="post">
-		<tr>
+    	<tr>
+    		<form action="" method="post">
 			<td><input type="hidden" name="id" value="<?php echo $row['id']; ?>"><?php echo $row['parent_item']; ?></td>
 			<td><?php echo $row['child_item']; ?></td>
 			<td><?php echo haalartikeltitelop($row['article_id']); ?></td>
-			<td width="200" href="javascript:;"
-				onClick="document.getElementById('<?php echo $row['id'];?>').style.display='block'"
-				style='cursor: pointer'><a>Wijzig</a>
+			<td width="200" ><input type="button" href="javascript:;" onClick="document.getElementById('<?php echo $row['id'];?>').style.display='block'" style='cursor: pointer' value="Wijzig">
 				<div id="<?php echo $row["id"];?>" style="display: none;">
+					
 					<select name="article_id" onChange="this.form.submit();">
     					<?php
-	$article = $row ['article_id'];
-	foreach ( $articles as $row ) {
-		$id = $row ["ID"];
-		$title = $row ["title"];
-		// Check which article is to be selected
-		if ($id == $article) {
-			$selected = " selected";
-		} else {
-			$selected = '';
-		}
-		print ("<option value=$id" . $selected . ">") ; // print articles
-		print ($title . "</option>") ;
-	}
-	?>
-						</select> <input type="submit" name="submit" value="Opslaan">
-				</div></td>
+						$article = $row ['article_id'];
+						foreach ( $articles as $row ) {
+							$id = $row ["ID"];
+								$title = $row ["title"];
+								// Check which article is to be selected
+								if ($id == $article) {
+									$selected = " selected";
+								} else {
+									$selected = '';
+								}
+								print ("<option value=\"$id\"" . $selected . ">") ; // print articles
+								print ($title . "</option>") ;
+							}
+							?>
+					</select> 
+					<input type="submit" name="submit" value="Opslaan">
+					
+				</div>
+			</td>
+			</form>
 		</tr>
-	</form>
+	
  	<?php
 }
 ?>
